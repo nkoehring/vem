118e7efd7985c1e2b543a4abf0343465a1d4af6f
koehr <n@koehr.in>
2018-02-20 23:43:32 +0100

    version push

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import Map from './Map'
 import Layer from './Layer'
-import Source from './Source'
 
 export default {
   install (Vue, options) {
     Vue.component('vem-map', Map)
     Vue.component('vem-layer', Layer)
-    Vue.component('vem-source', Source)
   }
 }
