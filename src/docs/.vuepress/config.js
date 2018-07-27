ceeb88b7e13bf89cc75f59d64e277a7e7f578a9e
koehr <n@koehr.in>
2018-07-27 16:54:31 +0200

    documentation update

---
+++ src/docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'VEM',
+  description: 'Vue Easy Maps',
+  base: '/docs',
+  dest: './docs',
+  serviceWorker: false,
+  evergreen: true,
+
+  markdown: {
+    lineNumbers: true
+  }
+}
--- src/docs/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import Documentation from './Documentation.vue'
-import './style.css'
-
-new Vue({
-  el: '#app',
-  render: h => h(Documentation)
-})
